094b8c73d5466ebd0d3ad25b0303846d1184a9fb
Paolo Patierno <ppatierno@live.com>
2019-03-08 14:39:54 +0100

    Removed deletion of old camel-output topic

--- kafka/10-undeploy-kafka.sh
+++ kafka/10-undeploy-kafka.sh
@@ -5,6 +5,5 @@ CLUSTER=${STRIMZI_CLUSTER:-my-cluster}
 
 # delete Kafka topics
 oc delete kafkatopic data-filtered -n $NAMESPACE
-oc delete kafkatopic camel-output -n $NAMESPACE
 # delete Kafka cluster
 oc delete kafka $CLUSTER -n $NAMESPACE
