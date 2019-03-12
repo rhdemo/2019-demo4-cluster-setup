484502324b02ab7a78d5e9842c661630e6ddafef
Hiram Chirino <hiram@hiramchirino.com>
2019-03-12 11:26:40 -0400

    Add syndesis deployment.

---
+++ syndesis/deploy.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+set -ex
+
+TARGET_PROJECT=camel-k-test
+oc new-project ${TARGET_PROJECT} | true
+
+#
+# Create some PVCs
+#
+oc replace --force -n ${TARGET_PROJECT} -f - <<EOF
+kind: PersistentVolumeClaim
+apiVersion: v1
+metadata:
+  name: syndesis-db
+spec:
+  accessModes:
+    - ReadWriteOnce
+  resources:
+    requests:
+      storage: 1Gi
+  storageClassName: gp2
+EOF
+
+oc replace --force -n ${TARGET_PROJECT} -f - <<EOF
+kind: PersistentVolumeClaim
+apiVersion: v1
+metadata:
+  name: syndesis-meta
+spec:
+  accessModes:
+    - ReadWriteOnce
+  resources:
+    requests:
+      storage: 1Gi
+  storageClassName: gp2
+EOF
+
+#
+# Install the CRD.
+#
+oc replace --force -n openshift -f https://raw.githubusercontent.com/syndesisio/syndesis/master/install/operator/deploy/syndesis-crd.yml
+
+#
+# Create the syndesis operator Deploytment Config, and create a Syndesis resoucre.
+#
+oc replace --force -n ${TARGET_PROJECT} -f https://raw.githubusercontent.com/syndesisio/syndesis/master/install/operator/deploy/syndesis-operator.yml 
+oc replace --force -n ${TARGET_PROJECT} -f - <<EOF
+apiVersion: "syndesis.io/v1alpha1"
+kind: "Syndesis"
+metadata:
+  name: "default"
+spec:
+  demoData: false
+  integration:
+    limit: 50
+    stateCheckInterval: 60
+  registry: docker.io
+EOF
