35aba489690c62238bfa890b199ee2893c59193e
Paolo Patierno <ppatierno@live.com>
2019-03-28 15:40:38 +0100

    Fixed Kafka topics to delete (#33)

--- kafka/10-undeploy-kafka.sh
+++ kafka/10-undeploy-kafka.sh
@@ -4,6 +4,7 @@ NAMESPACE=${STRIMZI_NAMESPACE:-strimzi}
 CLUSTER=${STRIMZI_CLUSTER:-my-cluster}
 
 # delete Kafka topics
-oc delete kafkatopic data-filtered -n $NAMESPACE
+oc delete kafkatopic sensorstream-ai -n $NAMESPACE
+oc delete kafkatopic sensorstream-raw -n $NAMESPACE
 # delete Kafka cluster
 oc delete kafka $CLUSTER -n $NAMESPACE
