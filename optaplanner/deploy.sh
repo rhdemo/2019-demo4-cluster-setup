5eda85980b64c488bdfe6b139f22c16bb1193f2f
Musa Talluzi <mtalluzi@redhat.com>
2019-04-08 16:31:25 -0400

    use template flag

--- optaplanner/deploy.sh
+++ optaplanner/deploy.sh
@@ -5,4 +5,4 @@ RESOURCE_DIR=$(dirname "$0")
 
 oc project ${PROJECT_NAMESPACE} 2> /dev/null || oc new-project ${PROJECT_NAMESPACE}
 oc create -f $RESOURCE_DIR/optaplanner-demo-template.yaml
-oc new-app $OPTAPLANNER_APP_NAME
+oc new-app --template="${PROJECT_NAMESPACE}/optaplanner-demo"
