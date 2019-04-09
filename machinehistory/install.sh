0506324bb59e5e7a2656d79be2d5721e28a855a5
Stuart Douglas <stuart@localhost.localdomain>
2019-04-09 22:25:45 +1000

    Updates to allow live coding in cluster

--- machinehistory/install.sh
+++ machinehistory/install.sh
@@ -7,7 +7,7 @@ oc new-project machine-history
 
 
 #setup the postgres instances
-oc new-app docker.io/swd847/postgres-dev --name=machine-history-postgres-dev
+oc new-app quay.io/redhatdemo/machine-history-postgres --name=machine-history-postgres-dev
 oc set env dc/machine-history-postgres-dev POSTGRES_USER=machine_history_user POSTGRES_PASSWORD=DSAf3DSdfhjkl39s9 POSTGRES_DB=machine_history_dev
 oc set volume dc/machine-history-postgres-dev --add --name=machine-history-postgres-dev-volume-1 -m /var/lib/postgresql --overwrite
 
@@ -20,8 +20,6 @@ oc secrets link builder machine-history-deployment-key
 
 oc new-app quarkus-jvm-s2i~git@github.com:rhdemo/2019-demo4-machine-history.git#final-state --context-dir=machine-history --name=machine-history-uberjar
 oc set env dc/machine-history-uberjar QUARKUS_DATASOURCE_URL=jdbc:postgresql://machine-history-postgres-dev:5432/machine_history_dev
-
-
 oc logs -f bc/machine-history-uberjar
 oc set build-secret --source bc/machine-history-uberjar machine-history-deployment-key
 oc start-build machine-history-uberjar
@@ -32,3 +30,17 @@ oc expose svc/machine-history-uberjar
 oc new-app --docker-image=quay.io/redhatdemo/machine-history --name=machine-history
 oc set env dc/machine-history QUARKUS_DATASOURCE_URL=jdbc:postgresql://machine-history-postgres-dev:5432/machine_history_dev
 oc expose svc/machine-history --hostname=machine-history.apps.dev.openshift.redhatkeynote.com
+
+
+
+oc new-build https://github.com/stuartwdouglas/quarkus-images.git --context-dir=centos-quarkus-jvm-dev-s2i --name quarkus-jvm-s2i-dev
+oc logs -f bc/quarkus-jvm-s2i-dev
+
+oc new-app quarkus-jvm-s2i-dev~git@github.com:rhdemo/2019-demo4-machine-history.git --context-dir=machine-history --name=machine-history-dev
+oc set env dc/machine-history-dev QUARKUS_DATASOURCE_URL=jdbc:postgresql://machine-history-postgres-dev:5432/machine_history_dev
+oc logs -f bc/machine-history-dev
+oc set build-secret --source bc/machine-history-dev machine-history-deployment-key
+oc start-build machine-history-dev
+oc logs -f bc/machine-history-dev
+oc expose svc/machine-history-dev --hostname=machine-history-dev.apps.dev.openshift.redhatkeynote.com
+oc annotate route svc/machine-history-dev --overwrite haproxy.router.openshift.io/timeout=2s
