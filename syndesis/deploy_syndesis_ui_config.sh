38bb7b5671b1d48e40dcc3220f193c6955faca28
lburgazzoli <lburgazzoli@gmail.com>
2019-04-16 12:16:57 +0200

    syndesis: update setup script

--- syndesis/deploy_syndesis_ui_config.sh
+++ syndesis/deploy_syndesis_ui_config.sh
@@ -12,7 +12,8 @@ oc new-project ${TARGET_PROJECT} | true
 # Patch syndesis-ui-config
 #
 oc get configmap syndesis-ui-config -o jsonpath={.data."config\.json"} \
-  | jq '.branding += { "logoWhiteBg": "", "logoDarkBg": "", "iconWhiteBg": "assets/images/FuseOnlineLogo_Black.svg", "iconDarkBg": "assets/images/FuseOnlineLogo_White.svg", "appName": "Ignite", "productBuild": true }' > /tmp/config.json
+  | jq '.branding += { "logoWhiteBg": "", "logoDarkBg": "", "iconWhiteBg": "assets/images/FuseOnlineLogo_Black.svg", "iconDarkBg": "assets/images/FuseOnlineLogo_White.svg", "appName": "Ignite", "productBuild": true }' \
+  > /tmp/config.json
 
 oc get configmap syndesis-ui-config --export -o json \
   | jq ". * $(oc create configmap syndesis-ui-config --from-file /tmp/config.json --dry-run -o json)" \
@@ -24,4 +25,4 @@ rm -f /tmp/config.json
 #
 # Restart syndesis-ui
 #
-oc delete pod -l syndesis.io/component=syndesis-ui
+#oc delete pod -l syndesis.io/component=syndesis-ui
