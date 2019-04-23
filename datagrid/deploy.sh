2597d99a19658fa5633c53196c1ff0f37a461522
Galder Zamarreño <galder@zamarreno.com>
2019-04-23 18:08:23 +0200

    Add missing GC parameters

--- datagrid/deploy.sh
+++ datagrid/deploy.sh
@@ -8,7 +8,12 @@ RESOURCE_DIR=$(dirname "$0")
 oc new-project datagrid-demo
 oc create -f $RESOURCE_DIR/datagrid-service-custom-xml.yaml
 oc create configmap datagrid-config --from-file=$RESOURCE_DIR/config
-oc new-app datagrid-service -p APPLICATION_USER=$USER  -p APPLICATION_PASSWORD=$PASS  -p NUMBER_OF_INSTANCES=$INSTANCES -p IMAGE=$IMAGE
+oc new-app datagrid-service \
+  -p APPLICATION_USER=$USER \
+  -p APPLICATION_PASSWORD=$PASS \
+  -p NUMBER_OF_INSTANCES=$INSTANCES \
+  -p IMAGE=$IMAGE \
+  -e JAVA_OPTS_APPEND="-XX:+PrintTenuringDistribution -XX:+PrintGCApplicationStoppedTime"
 oc expose svc/datagrid-service --name=console --port=console
 oc expose svc/datagrid-service --name=console-rest --path=/rest --port=http --hostname=$(oc get route console -o=go-template='{{ .spec.host }}')
 
