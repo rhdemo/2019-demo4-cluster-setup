16d641c6fe3aa042ea37798f5c41e8d483177f66
Paolo Patierno <ppatierno@live.com>
2019-04-30 14:19:52 +0200

    Echoing begin/end Kafka deployment

--- kafka/deploy.sh
+++ kafka/deploy.sh
@@ -8,6 +8,8 @@ oc project $STRIMZI_NAMESPACE 2> /dev/null || oc new-project $STRIMZI_NAMESPACE
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+echo "DEPLOYING KAFKA CLUSTER WITH STRIMZI ..."
+
 # cluster deployment
 $DIR/01-deploy-strimzi.sh
 $DIR/02-deploy-kafka.sh
@@ -15,3 +17,6 @@ $DIR/03-deploy-topics.sh
 $DIR/04-deploy-console-server.sh
 $DIR/05-deploy-console.sh
 $DIR/06-deploy-monitoring.sh
+
+echo ""
+echo "... KAFKA CLUSTER DEPLOYED!"
