5b7bc89e626f07b99e8be78035f407e652663890
lburgazzoli <lburgazzoli@gmail.com>
2019-05-03 22:49:06 +0200

    syndesis: improve damage game service

--- syndesis/services/load.groovy
+++ syndesis/services/load.groovy
@@ -1,11 +1,31 @@
-from('timer:inject?period={{timer.period:100}}')
+import org.apache.camel.AsyncCallback
+import org.apache.camel.AsyncProcessor
+import org.apache.camel.Exchange
+import org.apache.camel.Processor
+import org.apache.camel.Route
+import org.apache.camel.model.dataformat.JsonLibrary
+import org.apache.camel.support.RoutePolicySupport
+import org.apache.camel.util.AsyncProcessorHelper
+
+logger = org.slf4j.LoggerFactory.getLogger("damage-game")
+
+from('timer:inject?period={{timer.period:1s}}')
     .process {
         long counter   = it.properties.get(org.apache.camel.Exchange.TIMER_COUNTER, long.class)
         long machineId = counter % 10
 
-        it.message.body = """{ \"machineId\": ${machineId}, \"vibrationClass\": \"floss\" } """ as String
+        it.message.body = """{ \"machineId\": ${machineId}, \"vibrationClass\": \"x\" } """ as String
     }
     .to('seda:send?waitForTaskToComplete=Never')
 from('seda:send?concurrentConsumers={{seda.consumers:25}}')
-    .to('log:data')
-    .to("knative:endpoint/i-sensorstream-dumper")
+    .routePolicy(new RoutePolicySupport() {
+              @Override
+              public void onExchangeDone(Route route, Exchange exchange) {
+                logger.info('exchange duration={}, failed={}, data={}',
+                    System.currentTimeMillis() - exchange.created.time,
+                    exchange.exception != null,
+                    exchange.message.getBody(String.class)
+                )
+              }
+          })
+    .to("knative:endpoint/damage-game")
