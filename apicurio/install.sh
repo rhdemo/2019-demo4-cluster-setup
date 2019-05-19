24eee96edc4eb06fd4064286bf0cb89d23eb8c96
Stuart Douglas <stuart.w.douglas@gmail.com>
2019-05-20 09:40:45 +1000

    Remote Apicurio

--- apicurio/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-DB_USER=apicuriodb
-DB_PASS=adsfadsjkl923ksdaDSFa
-KC_USER=admin
-KC_PASS=quarkus
-RESOURCE_DIR=$(dirname "$0")
-oc new-project apicurio
-oc create -f $RESOURCE_DIR/apicurio-template.yaml
-oc new-app apicurio-studio-standalone -p KC_USER=$KC_USER -p KC_PASS=$KC_PASS
--- apicurio/remove.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-oc delete project apicurio
